84db27feebe5f175453a45e3c6b669460312a4cf
Loïc Darboux <loic.drbx@gmail.com>
2022-10-25 20:55:26 -0400

    return deployer address or vanity

--- HW3/Bootcamp.sol
+++ HW3/Bootcamp.sol
@@ -7,12 +7,24 @@ contract BootcampContract {
 
     uint256 number;
 
+    address deployer;
+
+    constructor() {
+        deployer = msg.sender;
+    }
+
     function store(uint256 num) public {
         number = num;
     }
 
-
-    function retrieve() public view returns (uint256){
+    function retrieve() public view returns (uint256) {
         return number;
+    }
+
+    function getDeployer() external view returns (address) {
+        if (msg.sender == deployer) {
+            return 0x000000000000000000000000000000000000dEaD;
+        }
+        return deployer;
     }
 }
